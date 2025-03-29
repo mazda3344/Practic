25076c443b3833af11a6749ac2967adcc56bb51c
Dima554815 <dimailin09.08@gmail.com>
2025-03-29 19:50:56 +0300

    script

--- Assets/Scipts/Practic1.cs
+++ Assets/Scipts/Practic1.cs
@@ -1,7 +1,15 @@
-
 using UnityEngine;
 
 public class Practic1 : MonoBehaviour
 {
+    private int n;
 
+
+    private void Start()
+    {
+        n = 19;
+        for (int i = 25; i>n; i--)
+        Debug.Log(i);
+    }
 }
+szr
