b5f753e1a5318eaa4c29d30659ab5180792288fc
Dima554815 <dimailin09.08@gmail.com>
2025-04-16 19:40:27 +0300

    practic6 /

--- Assets/Scipts/Practic6.cs
+++ Assets/Scipts/Practic6.cs
@@ -2,18 +2,23 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 public class Practic6 : MonoBehaviour
 {
     public TMP_Text TimerText;
     private float currentTime;
-    private float countdown = 60f;
+    public float countdown = 60f;
     void Start()
     {
-        
+        currentTime = countdown;
     }
     void Update()
     {
-        currentTime = Mathf.Round(Time.time);
+        currentTime -= Time.deltaTime;
         TimerText.text = currentTime.ToString();
+        if (currentTime <= 0) 
+        {
+            currentTime = countdown;
+        }
     }
 }
