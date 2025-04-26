0290f1478be8c5e989e2f590ad0230cac724f1a4
Dima554815 <dimailin09.08@gmail.com>
2025-04-26 19:45:10 +0300

    practic6 ttt

--- Assets/Scipts/Practic6.cs
+++ Assets/Scipts/Practic6.cs
@@ -1,30 +1,71 @@
-
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.UI;
 using System;
 public class Practic6 : MonoBehaviour
 {
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public int pin1 = 1;
+    public int pin2 = 1;
+    public int pin3 = 1;
+    public TMP_Text pin_1text;
+    public TMP_Text pin_2text;
+    public TMP_Text pin_3text;
     public float countdown = 60f;
     private float currentTime;
     public TMP_Text timerText;
     public Image image;
     private bool StopTime = false;
+    public Image imageh;
 
     void Start()
     {
+        button1.onClick.AddListener(OnButtonClick1);
+        button2.onClick.AddListener(OnButtonClick2);
+        button3.onClick.AddListener(OnButtonClick3);
         image.gameObject.SetActive(false);
         currentTime = countdown; 
         UpdateTimerText();
         StopTime = true;
     }
+    void OnButtonClick1()
+    {
+        pin1 += 1;
+        pin2 -= 1;
+        pin3 += 0;
+        pin_1text.text = pin1.ToString();
+        pin_2text.text = pin2.ToString();
+        pin_3text.text = pin3.ToString();
+        
+    }
+    void OnButtonClick2()
+    {
+        pin1 -= 1;
+        pin2 += 2;
+        pin3 -= 1;
+        pin_1text.text = pin1.ToString();
+        pin_2text.text = pin2.ToString();
+        pin_3text.text = pin3.ToString();
 
+    }
+    void OnButtonClick3()
+    {
+        pin1 -= 1;
+        pin2 += 1;
+        pin3 += 1;
+        pin_1text.text = pin1.ToString();
+        pin_2text.text = pin2.ToString();
+        pin_3text.text = pin3.ToString();
+
+    }
     void Update()
     {
         if (StopTime)
         {
             currentTime -= Time.deltaTime;
-
             if (currentTime <= 0)
             {
                 currentTime = 0;
@@ -32,7 +73,12 @@ public class Practic6 : MonoBehaviour
                 Debug.Log("ТЫ ПРОИГРАЛ");
                 image.gameObject.SetActive(true);
             }
-
+            if (pin1 == 0 && pin2 == 2 && pin3 == 2)
+            {
+                StopTime = false;
+                Debug.Log("ТЫ ПОБЕДИЛ");
+                imageh.gameObject.SetActive(true);
+            }
             UpdateTimerText();
         }
     }
--- Assets/Scipts/Practic62.cs
+++ Assets/Scipts/Practic62.cs
@@ -1,35 +1,104 @@
-    using UnityEngine;
+using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
-public class Practic62 : MonoBehaviour
+using UnityEngine.UI;
+
+public class practic6 : MonoBehaviour
 {
-    public TMP_Text Text1;
-    public TMP_Text Text2;
-    public TMP_Text Text3;
-    private int number1;
-    private int number2;
-    private int number3;
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public int pin1 = 1;
+    public int pin2 = 1;
+    public int pin3 = 1;
+    public TMP_Text pin_1text;
+    public TMP_Text pin_2text;
+    public TMP_Text pin_3text;
+    public float countdown = 60f;
+    private float currentTime;
+    public TMP_Text timerText;
+    public Image image;
+    private bool StopTime = false;
+    public Image imageh;
 
     void Start()
     {
-        number1 = 1;
-        number2 = 1;
-        number3 = 1;
-        UpdateText();
+        button1.onClick.AddListener(OnButtonClick1);
+        button2.onClick.AddListener(OnButtonClick2);
+        button3.onClick.AddListener(OnButtonClick3);
+        button4.onClick.AddListener(OnButtonClick4);
+        image.gameObject.SetActive(false);
+        currentTime = countdown; 
+        UpdateTimerText();
+        StopTime = true;
+    }
+    void OnButtonClick1()
+    {
+        pin1 += 1;
+        pin2 -= 1;
+        pin3 += 0;
+        pin_1text.text = pin1.ToString();
+        pin_2text.text = pin2.ToString();
+        pin_3text.text = pin3.ToString();
+        
+    }
+    void OnButtonClick2()
+    {
+        pin1 -= 1;
+        pin2 += 2;
+        pin3 -= 1;
+        pin_1text.text = pin1.ToString();
+        pin_2text.text = pin2.ToString();
+        pin_3text.text = pin3.ToString();
+
+    }
+    void OnButtonClick3()
+    {
+        pin1 -= 1;
+        pin2 += 1;
+        pin3 += 1;
+        pin_1text.text = pin1.ToString();
+        pin_2text.text = pin2.ToString();
+        pin_3text.text = pin3.ToString();
+
+    }
+    void OnButtonClick4()
+    {
+        countdown = 60f;
+        pin1 = 1;
+        pin2 = 1;
+        pin3 = 1;
+        StopTime = true;
+        pin_1text.text = pin1.ToString();
+        pin_2text.text = pin2.ToString();
+        pin_3text.text = pin3.ToString();
+
     }
     void Update()
     {
-        number1 += 1;
-        number2 -= 1;
-        number3 += 0;
-        UpdateText();
-
+        if (StopTime)
+        {
+            currentTime -= Time.deltaTime;
+            if (currentTime <= 0)
+            {
+                currentTime = 0;
+                StopTime = false;
+                Debug.Log("ТЫ ПРОИГРАЛ");
+                image.gameObject.SetActive(true);
+            }
+            if (pin1 == 7 && pin2 == 7 && pin3 == 7)
+            {
+                StopTime = false;
+                Debug.Log("ТЫ ПОБЕДИЛ");
+                imageh.gameObject.SetActive(true);
+            }
+            UpdateTimerText();
+        }
     }
-    private void UpdateText()
+
+    private void UpdateTimerText()
     {
-        Text1.text =  number1.ToString();
-        Text2.text = number2.ToString();
-        Text3.text = number3.ToString();
+        timerText.text = currentTime.ToString("F1");
     }
 }
- 
