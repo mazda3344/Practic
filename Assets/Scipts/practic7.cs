bf9e45a33f7b9d3568797e5ec1538f20847feec4
Dima554815 <dimailin09.08@gmail.com>
2025-05-07 19:41:46 +0300

    practic 7

--- Assets/Scipts/practic7.cs
+++ Assets/Scipts/practic7.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class practic7 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float MaxTime;
+    private Image img;
+    private float currentTime;
+
     void Start()
     {
-        
+        img = GetComponent<Image>();
+        currentTime = MaxTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        currentTime -= Time.deltaTime;
+
+        if (currentTime <= 0)
+            currentTime = MaxTime;
         
+        img.fillAmount = currentTime / MaxTime;
     }
 }
